d682564f6324715a766702d73188f26761d42c76
Shash <141257222+shash243@users.noreply.github.com>
2023-08-19 18:23:43 +0530

    Added a button to generate grid from user input.

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-mainDiv = document.querySelector('div.main');
-for (let i = 0; i < (16 * 16); i++){
-    divCell = document.createElement('div');
-    divCell.classList.add('cell');
-    divCell.style.cssText = `width: ${(640/17)}px; height: ${(640/17)}px`;
-    mainDiv.appendChild(divCell);
+function generateGrid() {
+    let num = Number(prompt("Enter number"));
+    const mainDiv = document.querySelector('div.main');
+    for (let i = 0; i < (num * num); i++) {
+        divCell = document.createElement('div');
+        divCell.classList.add('cell');
+        divCell.style.cssText = `width: ${Math.floor(960/ (num))}px; height: ${Math.floor(960 / (num))}px`;
+        mainDiv.appendChild(divCell);
+    }
+
+    cellDivs = document.querySelectorAll('div.cell');
+    cellDivs.forEach(div => {
+        div.addEventListener('mouseover', () => {
+            div.classList.add('hover');
+        })
+    });
 }
 
-cellDivs = document.querySelectorAll('div.cell');
-cellDivs.forEach(div => {
-    div.addEventListener('mouseover', ()=>{
-        div.classList.add('hover');
-    })
-});
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    generateGrid();
+})
